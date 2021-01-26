d51da1b37165ed0848cb48b7c4e3c966694a35d3
James Yang <gn00999829@gmail.com>
2021-01-26 01:43:19 -0800

    resolved conflict

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -72,8 +72,4 @@ export type MessageState = {
 export interface MessageContextProps {
   messageState: MessageState;
   messageDispatch: <T>(action: Payload<T>) => void;
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> 3ac187a609139ac02adffdbe6f1b5d5980ac338b
